b67c8887299614b15a2f6c6e2083ce06c29502b4
tuningfolk <alisahadges@gmail.com>
2023-01-08 12:22:45 +0530

    connect to api

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 from flask import Flask, render_template
+import urllib.request
+
+import os
+import requests
+import urllib.parse
+
+#Configuring application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True  
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    api_key = os.environ.get("API_KEY")
+    symbol = "INDIA"
+    url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={urllib.parse.quote(symbol)}&aqi=yes"
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    quote = response.json()
+    print(quote["location"]["name"])
+
+
+
+    return render_template("index.html")    
