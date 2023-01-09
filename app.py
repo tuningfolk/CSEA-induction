88f17a552b1e522cbc9300a0179085d23a22ace6
tuningfolk <alisahadges@gmail.com>
2023-01-09 12:33:02 +0530

    error handling

--- app.py
+++ app.py
@@ -22,39 +22,19 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def index():
     # api_key = os.environ.get("API_KEY")
     symbol = "NEW DELHI"
-    url = f"https://api.weatherapi.com/v1/current.json?key=8e67619d356c4ac7ad861838230801&q={urllib.parse.quote(symbol)}&aqi=yes"
-
-    response = requests.get(url)
-    response.raise_for_status()
-
-    quote = response.json()
-    is_day = int(quote["current"]["is_day"])
-    temp_c = quote["current"]["temp_c"]
-    temp_f = quote["current"]["temp_f"]
-
-    condition = quote["current"]["condition"]["text"]
-
-    wind_kph = quote["current"]["wind_kph"]
-    wind_degree = quote["current"]["wind_degree"]
-    wind_dir = quote["current"]["wind_dir"]
-
-    region = quote["location"]["name"]
-    location_country = quote["location"]["country"]
-
-    # print(quote["current"])
-
-    if(request.method == "POST"):
-        country_name = request.form.get("country_name")
-        symbol = country_name
-        print(symbol)
+    try:
         url = f"https://api.weatherapi.com/v1/current.json?key=8e67619d356c4ac7ad861838230801&q={urllib.parse.quote(symbol)}&aqi=yes"
-
         response = requests.get(url)
         response.raise_for_status()
+    except requests.RequestException:
+        return redirect("/")
 
+
+    
+    try:
         quote = response.json()
-        if not quote:
-            return redirect("/")
+    
+
         is_day = int(quote["current"]["is_day"])
         temp_c = quote["current"]["temp_c"]
         temp_f = quote["current"]["temp_f"]
@@ -67,6 +47,42 @@ def index():
 
         region = quote["location"]["name"]
         location_country = quote["location"]["country"]
+
+    except (KeyError, TypeError, ValueError):
+        return redirect("/")
+
+    if(request.method == "POST"):
+        country_name = request.form.get("country_name")
+        symbol = country_name
+        print(symbol)
+        try:
+            url = f"https://api.weatherapi.com/v1/current.json?key=8e67619d356c4ac7ad861838230801&q={urllib.parse.quote(symbol)}&aqi=yes"
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.RequestException:
+            return redirect("/")
+
+
+        
+        try:
+            quote = response.json()
+        
+
+            is_day = int(quote["current"]["is_day"])
+            temp_c = quote["current"]["temp_c"]
+            temp_f = quote["current"]["temp_f"]
+
+            condition = quote["current"]["condition"]["text"]
+
+            wind_kph = quote["current"]["wind_kph"]
+            wind_degree = quote["current"]["wind_degree"]
+            wind_dir = quote["current"]["wind_dir"]
+
+            region = quote["location"]["name"]
+            location_country = quote["location"]["country"]
+
+        except (KeyError, TypeError, ValueError):
+            return redirect("/")
 
 
 
